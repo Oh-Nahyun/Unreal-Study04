201db918be45fff658967f568bcf2705368f0663
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-10 19:39:00 +0900

    Update Player.h

--- Win32/04_Poop/Player.h
+++ Win32/04_Poop/Player.h
@@ -1,1 +1,30 @@
+/////
+
+#pragma once
+
+#include "Object.h"
+
+class CPlayer : public CObject
+{
+private:
+	INT		m_iHp;
+	INT		m_iAction;
+	FLOAT	m_fSpeed;
+
+public:
+	CPlayer();
+	virtual ~CPlayer();
+
+	void Init();
+	void Update();
+	void Render();
+	void Move();
+
+	int GetHP() { return m_iHp; }
+	float GetSpeed() { return m_fSpeed; }
+
+	void RefreshRect();
+	void SetDamage(int Damage) { m_iHp -= Damage; }
+	void ActionChange(int action);
+};
 
