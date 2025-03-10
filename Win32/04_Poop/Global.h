bd7dabb2ee4376b16056332ee777eb0aae250e45
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-10 19:37:12 +0900

    Update Global.h

--- Win32/04_Poop/Global.h
+++ Win32/04_Poop/Global.h
@@ -1,1 +1,9 @@
+/////
+
+#pragma once
+
+///// 전역 (extern)
+extern HWND			g_hWnd;
+extern HINSTANCE	g_hInst;
+extern LRESULT CALLBACK MsgProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
 
