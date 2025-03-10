75ea32e2f8b1de26a10063acc8126559ca893696
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-10 19:36:05 +0900

    Update Define.h

--- Win32/04_Poop/Define.h
+++ Win32/04_Poop/Define.h
@@ -1,1 +1,29 @@
+/////
+
+#pragma once
+
+#define WINSIZEX			800
+#define WINSIZEY			800
+#define RGB_VALUE			RGB(255, 0, 255)
+#define ENEMY_MAX			50
+
+///// 이미지 파일 인덱스, 8개의 이미지 파일
+enum {
+	IMG_BACK,
+	IMG_ALPHA,
+	IMG_MAP,
+	IMG_DDONG,
+	IMG_PLAYER,
+	IMG_PLAYER_LEFTRUN,
+	IMG_PLAYER_RIGHTRUN,
+	IMG_PLAYER_DEATH
+};
+
+///// 플레이어 행동 인덱스, 4가지 상태
+enum {
+	ACTION_STAND = 4,
+	ACTION_LEFT_RUN,
+	ACTION_RIGHT_RUN,
+	ACTION_DEATH
+};
 
