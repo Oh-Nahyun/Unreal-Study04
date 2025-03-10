11593f91bad26cb6cef63d25bbd83490a5a20f79
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-10 19:36:59 +0900

    Update GameManager.h

--- Win32/04_Poop/GameManager.h
+++ Win32/04_Poop/GameManager.h
@@ -1,1 +1,32 @@
+/////
+
+#pragma once
+
+#include "Image.h"
+#include "Player.h"
+#include "Enemy.h"
+
+class CGameManager
+{
+private:
+	CImageManager	m_Image;
+	CPlayer			m_Player;
+	CEnemyManager	m_Enemy;
+
+	DWORD			m_FrameTime;
+	DWORD			m_FrameTimeLimit;
+
+public:
+	CGameManager();
+	virtual ~CGameManager();
+	
+	bool Init();
+	bool Update();
+	bool Render();
+	
+	void ScoreCheck();
+	void Collision();
+	
+	void RenderInterface();
+};
 
